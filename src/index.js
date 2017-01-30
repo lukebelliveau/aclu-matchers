b1b90bc1e5da80ca7d4ea2a008cead5d0b6599d2
lukebelliveau <lbellive@thoughtworks.com>
2017-01-30 18:48:15 -0500

    added google analytics

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import GoogleAnalytics from 'react-ga';
+
 import App from './App';
 import './index.css';
 
 import { startCloudwok } from './Cloudwok'
+
+GoogleAnalytics.initialize('UA-91151405-1');
+
+function logPageView() {
+  GoogleAnalytics.set({ page: window.location.pathname })
+  GoogleAnalytics.pageView(window.location.pathname);
+}
 
 ReactDOM.render(
   <App />,
