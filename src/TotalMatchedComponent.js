4f45ff5b247439d4a5d34ee621e063e0e735c7e6
lukebelliveau <lbellive@thoughtworks.com>
2017-01-30 11:29:48 -0600

    updating totals

--- src/TotalMatchedComponent.js
+++ src/TotalMatchedComponent.js
@@ -9,9 +9,9 @@ const dateTimeStyle = {
   margin: 10
 };
 
-const amountMatched = '12,000';
+const amountMatched = '30,000';
 
-const timeEST = '11:00am';
+const timeEST = '12:00pm';
 
 const date = 'January 30, 2017';
 
