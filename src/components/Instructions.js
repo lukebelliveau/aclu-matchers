401fdb9e2fb2670753be13d5b99bed3595fd4e1c
Steven Liu <steve@stevenliu.com>
2017-01-31 22:09:28 +0000

    drop unused styles

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -13,18 +13,6 @@ const styles = {
     color: 'white',
     align: 'float right'
   },
-  wrapper: {
-    width: '800',
-    position: 'relative',
-    marginTop: '10px',
-  },
-  body: {
-    align: 'float left',
-    padding: 25,
-    borderTopStyle: 'solid',
-    margin: '0 auto',
-    width: '70%'
-  },
   link: {
     fontWeight: 'bold',
     paddingBottom: 10,
@@ -37,29 +25,27 @@ const styles = {
 }
 
 const Instructions = () => (
-  <div style={ styles.wrapper }>
-    <div style={ styles.body }>
-      <div style={ styles.border }>
+  <div >
 
-        <div>
-          <h1>Drag and drop your receipt below, and I will automatically tweet your receipt to at least ten matching donors.</h1>
-        </div>
-        <h2>This will multiply your donation by 10.</h2>
-        <h2><strong>Absolutely no personal information is required.</strong></h2>
-        <strong>Just make sure your screenshot shows your confirmation number and amount donated.</strong>
+    <div style={ styles.border }>
+      <div>
+        <h1>Drag and drop your receipt below, and I will automatically tweet your receipt to at least ten matching donors.</h1>
       </div>
-
-      <Cloudwok />
-
-      <h2>and if you haven't donated yet...</h2>
-      <div style={ styles.link }>
-        <h2>
-          <a style={ styles.a } href="https://action.aclu.org/secure/donate-to-aclu">Click here to donate to the <br /> American Civil Liberties Union</a>
-        </h2>
-      </div>
-      <h2>...then drop your receipt on the blue box to multiply it!</h2>
-
+      <h2>This will multiply your donation by 10.</h2>
+      <h2><strong>Absolutely no personal information is required.</strong></h2>
+      <strong>Just make sure your screenshot shows your confirmation number and amount donated.</strong>
     </div>
+
+    <Cloudwok />
+
+    <h2>and if you haven't donated yet...</h2>
+    <div style={ styles.link }>
+      <h2>
+        <a style={ styles.a } href="https://action.aclu.org/secure/donate-to-aclu">Click here to donate to the <br /> American Civil Liberties Union</a>
+      </h2>
+    </div>
+    <h2>...then drop your receipt on the blue box to multiply it!</h2>
+
   </div>
 );
 
