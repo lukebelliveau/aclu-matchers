a03ffd756319c07d3524168e3ee069437726aaf2
Steven Liu <steve@stevenliu.com>
2017-01-31 23:45:37 +0000

    pretty header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import {StyleSheet, css} from 'aphrodite';
+import {colors, whitespace} from '../styles/shared';
 
-const styles = {
-  // backgroundColor: '#222',
-  // height: '75px',
-  // padding: '20px',
-  // color: '#fff',
-}
+const s = StyleSheet.create({
+  headerPadding: {
+    padding: '.8rem',
+  },
+  white: {
+    color: '#fff'
+  },
+  titleShadow: {
+    textShadow: '1px 1px 1px black'
+  },
+  bgRed: colors.bgRed,
+  mb0: whitespace.margins.mb0,
+})
 
 const Header = () => (
-  <div style={styles}>
-    <h1>ACLU DONATION MULTIPLIER</h1>
+  <div className={css(s.headerPadding, s.bgRed)}>
+    <h1 className={css(s.white, s.titleShadow, s.mb0)}>ACLU DONATION MULTIPLIER</h1>
   </div>
 );
 
