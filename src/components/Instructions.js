81f6017ab92ddcd8e06f2da5d6bb6ba8bdd43bf1
Steven Liu <steve@stevenliu.com>
2017-01-31 23:20:26 +0000

    use aphrodite in Instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import {StyleSheet, css} from 'aphrodite';
 import Cloudwok from './Cloudwok';
 
 const styles = {
-  border: {
+  hero: {
     padding: '1rem',
     backgroundImage: `url('http://i.imgur.com/sAxC5zT.jpg')`,
     color: 'white',
   },
   link: {
     fontWeight: 'bold',
-    paddingBottom: 10,
-    position: 'relative'
   },
   a: {
     color: '#40B2F6',
@@ -18,10 +17,11 @@ const styles = {
   }  
 }
 
-const Instructions = () => (
-  <div >
+const s = StyleSheet.create(styles)
 
-    <div style={ styles.border }>
+const Instructions = () => (
+  <div>
+    <div className={css(s.hero)}>
       <h2>Drag and drop your receipt below, and I will automatically tweet your receipt to at least ten matching donors.</h2>
       <h3>This will multiply your donation by 10.</h3>
       <h3><strong>Absolutely no personal information is required.</strong></h3>
@@ -31,9 +31,9 @@ const Instructions = () => (
     <Cloudwok />
 
     <h2>and if you haven't donated yet...</h2>
-    <div style={ styles.link }>
+    <div className={css(s.link)}>
       <h2>
-        <a style={ styles.a } href="https://action.aclu.org/secure/donate-to-aclu">Click here to donate to the <br /> American Civil Liberties Union</a>
+        <a className={css(s.a)} href="https://action.aclu.org/secure/donate-to-aclu">Click here to donate to the <br /> American Civil Liberties Union</a>
       </h2>
     </div>
     <h2>...then drop your receipt on the blue box to multiply it!</h2>
