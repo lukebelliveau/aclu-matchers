6fe9615957aac4097c472e903856937736d2d797
Steven Liu <steve@stevenliu.com>
2017-01-31 21:15:28 +0000

    better html tags

--- src/StatementComponent.js
+++ src/StatementComponent.js
@@ -21,20 +21,23 @@ const wrapper = {
 const Statement = () => (
   <div className={ wrapper }>
     <div style={ body }>
-      <i>
+      <blockquote>
         “‘Extreme vetting’ is just a euphemism for discrimination against Muslims. Identifying specific countries with Muslim majorities and carving out exceptions for minority religions flies in the face of the constitutional principle that bans the government from either favoring or discriminating against particular religions. Any effort to discriminate against Muslims and favor other religions runs afoul of the First Amendment.”
-      </i>
+      </blockquote>
 
       <p style={signature}>
         - Anthony D. Romero, Executive Director, American Civil Liberties Union
       </p>
+
       <p>
         The above is a statement from the Executive Director of the ACLU, a statement that I wholeheartedly agree with - and I'm not alone.
-        <br/>
-        <br />
+      </p>
+      
+      <p>
         People around the nation have offered to match our donations to the ACLU; all they need from us is a tweet with the donation receipt.
-        <br/>
-        <br />
+      </p>
+      
+      <p>
         Drag and drop your receipt above, and I will automatically tweet your receipt to at least ten matching donors. Multiply your donation by 10!
       </p>
     </div>
