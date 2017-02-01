55fd64bf5badf6562ffee5daedd62b1622ff2305
lukebelliveau <lbellive@thoughtworks.com>
2017-02-01 12:13:01 -0500

    fixing statement - was not centered

--- src/components/Statement.js
+++ src/components/Statement.js
@@ -1,35 +1,39 @@
 import React from 'react';
-import {StyleSheet, css} from 'aphrodite';
-import {container, colors, font, text, whitespace} from '../styles/shared';
 
-const s = StyleSheet.create({
-  italic: font.style.italic,
-  xlarge: text.size.xlarge,
-  serif: font.family.serif,
-  bgDarkBlue: colors.bgDarkBlue,
-  offWhite: colors.white,
-  p8: whitespace.paddings.p8,
-  container,
-  right: text.align.right,
-  large: text.size.large,
-  textShadow: text.shadow.one,
-})
+const signature = {
+  fontSize: '0.8em'
+};
+
+const body = {
+  align: 'float left',
+  padding: 25,
+  borderTopStyle: 'solid',
+  borderBottomStyle: 'solid',
+  margin: '0 auto',
+  width: '70%'
+};
+
+const wrapper = {
+  width: '800',
+  position: 'relative'
+};
 
 const Statement = () => (
-  <div className={css(s.bgDarkBlue, s.offWhite, s.p8, s.textShadow)}>
-    <div className={css(s.container)}>
-      <blockquote className={css(s.italic, s.xlarge, s.serif)}>
+  <div className={ wrapper }>
+    <div style={ body }>
+      <i>
         “‘Extreme vetting’ is just a euphemism for discrimination against Muslims. Identifying specific countries with Muslim majorities and carving out exceptions for minority religions flies in the face of the constitutional principle that bans the government from either favoring or discriminating against particular religions. Any effort to discriminate against Muslims and favor other religions runs afoul of the First Amendment.”
-      </blockquote>
+      </i>
 
-      <p className={css(s.right, s.large)}>
+      <p style={signature}>
         - Anthony D. Romero, Executive Director, American Civil Liberties Union
       </p>
-
-      <p>  
+      <p>
         The above is a statement from the Executive Director of the ACLU, a statement that I wholeheartedly agree with - and I'm not alone.
+        <br/>
         <br />
         People around the nation have offered to match our donations to the ACLU; all they need from us is a tweet with the donation receipt.
+        <br/>
         <br />
         Drag and drop your receipt above, and I will automatically tweet your receipt to at least ten matching donors. Multiply your donation by 10!
       </p>
