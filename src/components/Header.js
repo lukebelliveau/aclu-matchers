cb03e46927fe59f1810ae1b49a4e9f9f753e5584
Robin Doherty <rdoherty@gmail.com>
2017-02-03 08:45:39 +1000

    fetch image over active protocol to fix mixed content warning on HTTPS

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const style = StyleSheet.create({
   mb0: whitespace.margins.mb0,
   header: {
     padding: '20px',
-    background: `url('http://i.imgur.com/sAxC5zT.jpg')`,
+    background: `url('//i.imgur.com/sAxC5zT.jpg')`,
     backgroundSize: '100%'
   }
 })
