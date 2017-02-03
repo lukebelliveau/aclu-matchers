1af55b4fcac0cbbdf1e8ad5d62b195f264672580
Rufus Raghunath <rraghun@thoughtworks.com>
2017-02-03 16:46:56 +0100

    RR - pull TotalMatched into Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import {StyleSheet, css} from 'aphrodite';
 import {colors, whitespace} from '../styles/shared';
+import TotalMatched from './TotalMatched';
+
 
 const style = StyleSheet.create({
   white: colors.white,
   mb0: whitespace.margins.mb0,
   header: {
-    padding: '20px',
+    paddingTop: '20px',
     background: `url('//i.imgur.com/sAxC5zT.jpg')`,
     backgroundSize: '100%'
+  },
+  title: {
+    marginBottom: '20px'
   }
-})
+});
 
 const Header = () => (
   <div className={css(style.header, style.bgBlue)}>
-    <h1 className={css(style.white, style.mb0)}>ACLU Donation Multiplier</h1>
+    <h1 className={css(style.white, style.mb0, style.title)}>ACLU Donation Multiplier</h1>
+    <TotalMatched />
   </div>
 );
 
