b63f505c532726ef6d54479656c8872f1eec7e7d
Lucas Hanke <lhanke@thoughtworks.com>
2017-02-03 13:35:05 -0200

    add donation validator

---
+++ src/validators/OCRValidator.js
@@ -0,0 +1,12 @@
+import Tesseract from 'tesseract.js/dist/tesseract';
+
+const validateReceipt = async file => {
+  let valid = null;
+  await Tesseract.recognize(file).then(result => {
+    valid = result.text.toLowerCase().includes('donation');
+  });
+  console.log('OCR Validation result: ' + (valid ? 'valid' :' invalid'));
+  return valid;
+};
+
+export default validateReceipt;
