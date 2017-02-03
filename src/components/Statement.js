f0bc36f546daa3b25ac40356f9eadc0b38a1ae19
Glauber Ramos <glauberamos@gmail.com>
2017-02-03 16:42:26 -0200

    new layout for why-donate section

--- src/components/Statement.js
+++ src/components/Statement.js
@@ -7,21 +7,36 @@ const signature = {
 
 const styles = {
   wrapperStyle: {
-    background: '#8ddbff',
     padding: '50px',
-    marginBottom: '30px',
+    paddingTop: 0,
     '@media (max-width: 425px)': {
       padding: '0px',
      }
   },
   body: {
+    borderBottom: '2px solid #eee',
     padding: 25,
+    paddingBottom: 60,
     margin: '0 auto',
     width: '70%',
     '@media (max-width: 425px)': {
       padding: '15px',
       width: '90%'
      }
+  },
+  h2: {
+    textAlign: 'left',
+    margin: '30px 0'
+  },
+  p: {
+    textAlign: 'left',
+    marginTop: 30,
+    marginBottom: 0
+  },
+  strong: {
+    textAlign: 'left',
+    display: 'block',
+    marginTop: 30
   }
 }
 
@@ -30,21 +45,18 @@ const style = StyleSheet.create(styles)
 const Statement = () => (
   <div className={css(style.wrapperStyle)}>
     <div className={css(style.body)}>
-      <i>
-        “‘Extreme vetting’ is just a euphemism for discrimination against Muslims. Identifying specific countries with Muslim majorities and carving out exceptions for minority religions flies in the face of the constitutional principle that bans the government from either favoring or discriminating against particular religions. Any effort to discriminate against Muslims and favor other religions runs afoul of the First Amendment.”
-      </i>
-
-      <p style={signature}>
-        - Anthony D. Romero, Executive Director, American Civil Liberties Union
-      </p>
-      <p>
-        The above is a statement from the Executive Director of the ACLU, a statement that I wholeheartedly agree with - and I'm not alone.
-        <br/>
-        <br />
-        People around the nation have offered to match our donations to the ACLU; all they need from us is a tweet with the donation receipt.
-        <br/>
-        <br />
-        Drag and drop your receipt above, and I will automatically tweet your receipt to at least ten matching donors. Multiply your donation by 10!
+      <h2 className={css(style.h2)}>Why donate?</h2>
+      <blockquote>
+        <p>
+        Extreme vetting is just a euphemism for discrimination against Muslims. Identifying specific countries with Muslim majorities and carving out exceptions for minority religions flies in the face of the constitutional principle that bans the government from either favoring or discriminating against particular religions. Any effort to discriminate against Muslims and favor other religions runs afoul of the First Amendment.
+        </p>
+        <footer>
+          - Anthony D. Romero, Executive Director, American Civil Liberties Union
+        </footer>
+      </blockquote>
+      <strong className={css(style.strong)}>We wholeheartedly agree with this statement - and we're not alone.</strong>
+      <p className={css(style.p)}>
+        People around the nation have offered to match our donations to the ACLU; all they need from us is a tweet with the donation receipt.  Among others, we have matched donations with Ajay Chopra, Sia, and Jesse Tyler.
       </p>
     </div>
   </div>
