88d13bea2603e58f37fd59273d34e5886af6a927
Steven Liu <steve@stevenliu.com>
2017-02-08 22:30:50 +0000

    sypl - make shared styles js file so can add comments

---
+++ src/styles/shared.js
@@ -0,0 +1,60 @@
+// colours
+export const white = {color: "#fff"}
+
+// margins
+export const m0 = {margin: 0}
+export const mt0 = {marginTop: 0}
+export const mr0 = {marginRight: 0}
+export const mb0 = {marginBottom: 0}
+export const ml0 = {marginLeft: 0}
+
+// paddings
+export const p0 = {padding: 0}
+export const pt0 = {paddingTop: 0}
+export const pr0 = {paddingRight: 0}
+export const pb0 = {paddingBottom: 0}
+export const pl0 = {paddingLeft: 0}
+export const p8 = {padding: "8px"}
+export const pt8 = {paddingTop: "8px"}
+export const pr8 = {paddingRight: "8px"}
+export const pb8 = {paddingBottom: "8px"}
+export const pl8 = {paddingLeft: "8px"}
+
+// background properties
+export const backgroundCover = {backgroundSize: "cover"}
+
+// text align
+export const alignLeft = {textAlign: "left"}
+export const alignRight = {textAlign: "right"}
+export const alignCenter = {textAlign: "center"}
+
+// text styles
+export const bold = {fontWeight: "bold"}
+export const italic = {fontStyle: "italic"}
+export const noDecoration = {textDecoration: "none"}
+
+// text shadows
+export const textShadow1 = {textShadow: "1px 1px 1px #000"}
+export const textShadow2 = {textShadow: "2px 2px 1px #000"}
+
+// fonts
+export const fontFamilyHeadingRegular = {fontFamily: "heading_regular"}
+export const fontFamilyHeadingBold = {fontFamily: "heading_bold"}
+export const fontFamilyParagraphRegular = {fontFamily: "paragraph_regular"}
+export const fontFamilyParagraphBold = {fontFamily: "paragraph_bold"}
+export const fontFamilyParagraphLight = {fontFamily: "paragraph_light"}
+
+// font sizes
+export const smaller = {fontSize: ".6rem"}
+export const small = {fontSize: ".8rem"}
+export const medium = {fontSize: "1rem"}
+export const large = {fontSize: "1.2rem"}
+export const larger = {fontSize: "1.4rem"}
+export const xlarge = {fontSize: "1.6rem"}
+export const xxlarge = {fontSize: "1.8rem"}
+
+// composite styles
+export const container = {
+  "maxWidth": "1024px",
+  "margin": "0 auto"
+}
